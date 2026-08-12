83908868ebd8a1d96de4a94fae65772ff2210aa6
Aaravramawat <Aarav.37088@stu.upes.ac.in>
2026-08-12 23:54:59 +0530

    Add files via upload

---
+++ day 3 question 2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3;
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    printf("\nBefore swapping: num1 = %d, num2 = %d\n", num1, num2);
+
+    num3 = num1; 
+    num1 = num2;   
+    num2 = num3;  
+
+    printf("After swapping: num1 = %d, num2 = %d\n", num1, num2);
+    return 0;
+}
