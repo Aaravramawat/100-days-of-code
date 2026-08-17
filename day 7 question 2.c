b8321edf2298b4195365976bf7d257c387cbf019
Aaravramawat <Aarav.37088@stu.upes.ac.in>
2026-08-17 17:24:57 +0530

    Add files via upload

---
+++ day 7 question 2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    
+    if ((ch >= 'a' && ch <= 'z')) {
+        
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            printf("%c is a vowel.\n", ch);
+        } else {
+            printf("%c is a consonant.\n", ch);
+        }
+        
+    } else {
+        printf("%c is not a valid alphabet letter.\n", ch);
+    }
+
+    return 0;
+}
