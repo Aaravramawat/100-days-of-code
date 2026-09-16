c28846d233c4b85e093d228dbe41699a9ece7bd3
Aaravramawat <Aarav.37088@stu.upes.ac.in>
2026-09-16 23:35:11 +0530

    Add files via upload

---
+++ day 38 question 1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main()
+{
+    int r, c;
+    int a[10][10], b[10][10], sum[10][10];
+
+    scanf("%d %d", &r, &c);
+
+    // Input first matrix
+    for(int i = 0; i < r; i++)
+    {
+        for(int j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Input second matrix
+    for(int i = 0; i < r; i++)
+    {
+        for(int j = 0; j < c; j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Add matrices
+    for(int i = 0; i < r; i++)
+    {
+        for(int j = 0; j < c; j++)
+        {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    // Print result
+    for(int i = 0; i < r; i++)
+    {
+        for(int j = 0; j < c; j++)
+        {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
