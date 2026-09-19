4d86eeaaba69be443f598a132e358c682788b9dc
Aaravramawat <Aarav.37088@stu.upes.ac.in>
2026-09-19 21:34:19 +0530

    Add files via upload

---
+++ day 39 question 2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10];
+    int n, sum = 0;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        sum = sum + a[i][i];
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
