dd08bd4d8463548dd2984767bd09641c2918da19
Aaravramawat <Aarav.37088@stu.upes.ac.in>
2026-09-24 21:07:39 +0530

    Add files via upload

---
+++ day 46 question 2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int count[26] = {0};
+
+    fgets(str, sizeof(str), stdin);
+
+    for(int i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] >= 'a' && str[i] <= 'z')
+        {
+            count[str[i] - 'a']++;
+
+            if(count[str[i] - 'a'] == 2)
+            {
+                printf("%c", str[i]);
+                return 0;
+            }
+        }
+    }
+
+    printf("-1");
+
+    return 0;
+}
